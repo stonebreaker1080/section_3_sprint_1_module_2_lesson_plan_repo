033dd3c066eb83e999c9d86f158576c5899740f7
Kyle Hauenstein <36925697+stonebreaker1080@users.noreply.github.com>
2022-05-11 20:34:24 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> Welcome to Friends! </h1>
+        <p>
+          The purpose of this follow along assignment is to give you access to
+          more 'at bats' with ReactJS and to talk about the pieces of the{" "}
+          <strong>React Component LifeCycle</strong> along the way.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
